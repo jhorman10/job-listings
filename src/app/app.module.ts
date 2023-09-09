fe56029c801fd3d7a5795c1b77a20ed84d174344
Jhorman Orozco <jsojhor@gmail.com>
2023-09-09 09:20:05 -0500

    fix responsive and add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
 import { FilterComponent } from './components/filter/filter.component';
 import { JobListService } from './services/job-list.service';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    FilterComponent
+    FilterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
