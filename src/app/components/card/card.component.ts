7d8baff8a44371fdf394b15ba135c0777180e7ec
Jhorman Orozco <jsojhor@gmail.com>
2023-09-09 16:35:43 -0500

    ready to interview

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,11 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { Card } from 'src/app/models/card.model';
 
-
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: [ './card.component.scss' ],
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
   @Input() cardData!: Card;
--- src/app/components/filter-item/filter-item.component.ts
+++ src/app/components/filter-item/filter-item.component.ts
@@ -1,10 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-filter-item',
   templateUrl: './filter-item.component.html',
-  styleUrls: ['./filter-item.component.scss']
+  styleUrls: ['./filter-item.component.scss'],
 })
 export class FilterItemComponent {
   @Input() filter!: string;
+  @Output() filterRemoved = new EventEmitter<string>();
+
+  removeItem() {
+    this.filterRemoved.emit(this.filter);
+  }
 }
