ba80550cb40467bfdd1bd1d9349ae02b172ba5dd
Jhorman Orozco <jsojhor@gmail.com>
2023-09-09 08:35:49 -0500

    card works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { JobListService } from './services/job-list.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [JobListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/job-list.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Card } from '../models/card.model';
+@Injectable({
+  providedIn: 'root',
+})
+export class JobListService {
+  constructor(private http: HttpClient) {}
+
+  getJobListData(): Observable<Card[]> {
+    return this.http.get<Card[]>('../../assets/data/data.json');
+  }
+}
